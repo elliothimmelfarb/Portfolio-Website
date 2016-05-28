7d50c1c1debf93a33732d2a5cba6e6ea65caf1f8
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-05-28 16:31:23 -0700

    responsive work and contact icon links added

--- ElliotHimmelfarb.js
+++ ElliotHimmelfarb.js
@@ -1,5 +1,25 @@
 $(document).ready(init());
 
 function init() {
-    console.log("working");
+    dimensionSet();
+
+    var navbarTop = $(".nav");
+    var navButtons = $(".navB");
+    var scrollPage = $(".scroll-page");
+
+    function dimensionSet() {
+        if ($(navbarTop).height() > 100) {
+            $(scrollPage).css("padding-top", 150);
+            $()
+        } else {
+            $(scrollPage).css("padding-top", 45);
+        }
+    }
+
+    dimensionSet();
+
+    $(window).on("resize", function () {
+        console.log($(navbarTop).height());
+        dimensionSet();
+    })
 }
